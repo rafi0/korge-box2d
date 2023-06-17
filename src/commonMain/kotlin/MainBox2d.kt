c6ce661a62e26627d815f75772ae4038a8e22697
rafi0 <rafalziemniewski@wp.pl>
2023-06-17 10:18:56 +0200

    scene to reproduce the linearVelocity problem

--- src/commonMain/kotlin/MainBox2d.kt
+++ src/commonMain/kotlin/MainBox2d.kt
@@ -1,33 +1,38 @@
+import korlibs.korge.box2d.*
 import korlibs.korge.scene.*
 import korlibs.korge.view.*
-//import korlibs.korge.view.ktree.*
-import korlibs.io.file.std.*
-import korlibs.korge.box2d.*
 import korlibs.math.geom.Anchor
-import korlibs.math.geom.Size
 import org.jbox2d.dynamics.BodyType
 
 class MainBox2d : Scene() {
-    override suspend fun SContainer.sceneMain() {
-        //registerBox2dSupportOnce()
-        //addChild(resourcesVfs["restitution.ktree"].readKTree(views))
-        //registerBox2dSupportOnce()
+  override suspend fun SContainer.sceneMain() {
+    //registerBox2dSupportOnce()
+    //addChild(resourcesVfs["restitution.ktree"].readKTree(views))
+    //registerBox2dSupportOnce()
 
-        solidRect(920, 100).xy(0, 620).registerBodyWithFixture(type = BodyType.STATIC, friction = 0.2, restitution = 0.2)
-        for (n in 0 until 5) {
-            //fastEllipse(Size(100, 100))
-            circle(50f)
-            //ellipse(Size(50, 50))
-                .xy(120 + 140 * n, 246)
-                .anchor(Anchor.CENTER)
-                .registerBodyWithFixture(
-                    type = BodyType.DYNAMIC,
-                    linearVelocityY = 6.0,
-                    friction = 0.2,
-                    restitution = 0.3 + (n * 0.1)
-                )
+    solidRect(920, 100).xy(0, 620).registerBodyWithFixture(type = BodyType.STATIC, friction = 0.2, restitution = 0.2)
+    for (n in 0 until 5) {
+      //fastEllipse(Size(100, 100))
+      val c = circle(50f)
+        //ellipse(Size(50, 50))
+        .xy(120 + 140 * n, 246)
+        .anchor(Anchor.CENTER)
+        .registerBodyWithFixture(
+          type = BodyType.DYNAMIC,
+          linearVelocityX = 16.0,
+          friction = 0.2,
+          restitution = 0.3 + (n * 0.1)
+        )
+      val enableHackyWorkaround = true
+      if (enableHackyWorkaround) {
+        onNextFrame {
+          onNextFrame {
+            c.body!!._linearVelocity.x = 16f
+          }
         }
+      }
     }
+  }
 }
 
 /*
